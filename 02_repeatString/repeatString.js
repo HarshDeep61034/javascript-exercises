0978f64be0f88cb262a36168344254bc1227fa03
HarshDeep61034 <hdeep61034@gmail.com>
2023-02-21 21:14:15 +0530

    solved question 01 & 02 js by the odin project

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    var str = string;
+    if(num > 0){
+for(let i = 1; i<num; i++ ){
+    str += string;
+}
+    
+return str
+    }
+    else if(num === 0){
+        return "";
+    }
+    else{
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
