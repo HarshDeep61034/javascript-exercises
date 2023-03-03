b4c8699c878a44e5a42ed3d794bb35ed1a5fe7ff
HarshDeep61034 <hdeep61034@gmail.com>
2023-03-03 16:07:09 +0530

    solved 06 problem of leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function (Year) {
+    if (Year % 4 === 0) {
+        if (Year % 100 === 0) {
+            if (Year % 400 === 0) {
+                return true
+            }
+            else {
+                return false
+            }
+        }
+        else {
+            return true
+        }
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
