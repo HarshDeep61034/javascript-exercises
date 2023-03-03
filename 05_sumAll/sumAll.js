180dee1e80857ba5e17c81cbb9427fc506f0236d
HarshDeep61034 <hdeep61034@gmail.com>
2023-03-03 15:47:00 +0530

    solved problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 || typeof x !== 'number' || typeof y !== 'number') return "ERROR";
+    if(y > x){
+      var a = x;
+      var b = y;
+    }
+    else{
+      var b = x;
+      var a = y;
+    }
+      var sum = 0;
+      for(let i = a; i<=b; i++){
+        sum += i;
+      }
+      return sum
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
