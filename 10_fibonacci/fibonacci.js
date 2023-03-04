1bb86827d6459cba095b9bcf1f7a32843e968acd
HarshDeep61034 <hdeep61034@gmail.com>
2023-03-04 10:05:38 +0530

    solved Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+    let result = 0;
+    let arr = [];
+    if(x<0){
+        return "OOPS"
+      }
+    for (let i = 1; i <= x; i++) {
+        if (i < 3) {
+            arr.splice(0, 0, 1);
+        }
+        else if (i >= 3) {
+            for (let x in arr) {
+                result = arr[0] + arr[1];
+            }
+            arr.splice(0, 0, result);
+        }
+    }
+    return arr[0];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
