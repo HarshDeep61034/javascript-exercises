7984212b11dd2319deb2c63b6a1fe9bec3da6042
HarshDeep61034 <hdeep61034@gmail.com>
2023-03-04 11:54:02 +0530

    solved problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,50 @@
-const findTheOldest = function() {
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 2018,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
+const findTheOldest = function (people) {
+    function diff(a, b) {
+        return a - b;
+    }
+    let arr = [];
+    const x = people.length
+    for (let i = 0; i < x; i++) {
+        if (typeof people[i].yearOfDeath === 'undefined') {
+            const today = new Date();
+            const year = today.getFullYear();
+            people[i].yearOfDeath = year;
+        }
+        var yearOfDeath = people[i].yearOfDeath
+        var yearOfBirth = people[i].yearOfBirth
+        const agespan = yearOfDeath - yearOfBirth;
+        arr.push(agespan);
+    }
 
+    arr.sort(diff);
+    arr.reverse();
+    console.log(arr);
+    let maxAge = arr[0];
+    for (let i = 0; i < x; i++) {
+        var yearOfDeath = people[i].yearOfDeath
+        var yearOfBirth = people[i].yearOfBirth
+        const agespan = yearOfDeath - yearOfBirth;
+        if (maxAge == agespan) {
+            var person = people[i];
+        }
+    }
+    return person;
 };
 
 // Do not edit below this line
