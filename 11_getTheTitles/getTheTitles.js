10a3eee4e88df5c12aecbc5834518c1229212dae
HarshDeep61034 <hdeep61034@gmail.com>
2023-03-04 10:36:08 +0530

    solved problem no 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    const x = books.length;
+    let arr = [];
+    for (let i = 0; i < x; i++) {
+       arr.push(books[i].title);
+    }
+    return arr;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
